313e6c127d4a4910845188ad8eeb2ce599af306b
Fuyan777 <fuyamiwa713@gmail.com>
2021-11-08 02:41:32 +0900

    fix

--- app.py
+++ app.py
@@ -57,8 +57,14 @@ def message_text(event):
         message_send = "後悔して落ち込んでてもね仕方ないんだよ！ねぇ知ってた！？"
     elif message_received == "文化祭":
         message_send = "だから今年は絶対優勝するの！最後だもん"
+    elif message_received == "カワイ子ちゃん":
+        message_send = "ムゥー、嫌っ！"
+    elif message_received == "あきらめる":
+        message_send = "あ、聞いて、知ってる、昔、挫折しちゃってヒーロー諦めちゃって問題起こしちゃった子がいたんだよ、知ってた！？"
+    elif message_send == "大変":
+        message_send = "大変だよねえ、ちゃんと考えないと辛いよ、これは辛いよー"
     else:
-        message_send = "ねぇなんで" + message_received + "って言うの？不思議！"
+        message_send = "ねぇなんで、「" + message_received + "」って言うの？不思議！"
 
     line_bot_api.reply_message(
         event.reply_token,
